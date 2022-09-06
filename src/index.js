fa7592edf0ef5f38608a27d8dfd8f3d5a28adb4f
Nikita Pozhidaev <pozhidaev2002@inbox.ru>
2022-09-06 23:01:53 +0700

    update: index.js - test code, .eslintrc - rules, env

--- src/index.js
+++ src/index.js
@@ -3,13 +3,34 @@ import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-function tick() {
-  const element = (
-    <div>
-      <h2>Сейчас {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  root.render(element);
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Привет, мир!</h1>
+        <h2>Сейчас {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
 
-setInterval(tick, 1000);
+root.render(<Clock />);
