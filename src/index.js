f7991867a4fa7145efaa05eb4fc72e2a1ce83e9f
Nikita Pozhidaev <pozhidaev2002@inbox.ru>
2022-09-08 18:04:09 +0700

    add: lifting-sate-up as an example degrees

--- src/index.js
+++ src/index.js
@@ -69,6 +69,7 @@ class FlaworForm extends React.Component {
   }
 
   handleSumbit(event) {
+    // Eslint ноет что это хрень не юзай ее
     alert(`Ваш любимый вкус: ${this.state.value}`);
     event.preventDefault();
   }
@@ -91,3 +92,105 @@ class FlaworForm extends React.Component {
 }
 
 root.render(<FlaworForm />);
+
+// Объект с полным названием систем измерений
+const scaleNames = {
+  c: "Celsius",
+  f: "Fahrenheit",
+};
+
+// Две функции для вычисления градусов противоположенной системы исчисления
+function toCelsius(fahrenheit) {
+  return ((fahrenheit - 32) * 5) / 9;
+}
+
+function toFahrenheit(celsius) {
+  return (celsius * 9) / 5 + 31;
+}
+
+// Функция вызывается в Calculator для вычисления и конвертации значения
+function tryConvert(temperature, convert) {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) {
+    return "";
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
+
+function BoilingVerdict(props) {
+  if (props.celsius >= 100) {
+    return <p>The water would boil.</p>;
+  }
+  return <p>The water would not boil.</p>;
+}
+
+// Класс который вызывается в Calculator и ему передается props с значением из state Calculator
+class TemperatureInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.props.onTemperatureChange(e.target.value);
+  }
+
+  render() {
+    // Eslint Диструктуризация равнасильна const name = this.state.name
+    const { temperature, scale } = this.props;
+    return (
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[scale]}:</legend>
+        <input value={temperature} onChange={this.handleChange} />
+      </fieldset>
+    );
+  }
+}
+
+// Класс который хранит в себе state и передает их своим дочерним элементам
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+    this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+    this.state = { temperature: "", scale: "c" };
+  }
+
+  // Две функции которые вызываются в render()
+  handleCelsiusChange(temperature) {
+    this.setState({ scale: "c", temperature });
+  }
+
+  handleFahrenheitChange(temperature) {
+    this.setState({ scale: "f", temperature });
+  }
+
+  render() {
+    // Eslint Диструктуризация равнасильна const name = this.state.name
+    const { scale, temperature } = this.state;
+    const celsius =
+      scale === "f" ? tryConvert(temperature, toCelsius) : temperature;
+    const fahrenheit =
+      scale === "c" ? tryConvert(temperature, toFahrenheit) : temperature;
+
+    return (
+      <div>
+        <TemperatureInput
+          scale="c"
+          temperature={celsius}
+          onTemperatureChange={this.handleCelsiusChange}
+        />
+        <TemperatureInput
+          scale="f"
+          temperature={fahrenheit}
+          onTemperatureChange={this.handleFahrenheitChange}
+        />
+        <BoilingVerdict celsius={parseFloat(celsius)} />
+      </div>
+    );
+  }
+}
+
+root.render(<Calculator />);
