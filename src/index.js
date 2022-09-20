d38f0c4edf3b942ffcaea18eac3e42b995f6db34
Nikita Pozhidaev <pozhidaev2002@inbox.ru>
2022-09-20 22:55:51 +0700

    add: connected test scss style

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./style.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function Button() {
+  return <button className="color_button">Button</button>;
+}
+
+root.render(<Button />);
