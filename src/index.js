927e65e98c8aeefa6c96e48b3923bc8ea0720e97
Nikita Pozhidaev <pozhidaev2002@inbox.ru>
2022-09-22 15:41:13 +0700

    add: test updat theme

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./style.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+const themes = {
+  dark: "dark",
+  light: "",
+};
+
 function Button() {
-  return <button className="color_button">Button</button>;
+  const [theme, setTheme] = useState(themes.dark);
+
+  function onUpdateTheme() {
+    if (theme === themes.dark) setTheme(themes.light);
+    if (theme === themes.light) setTheme(themes.dark);
+  }
+
+  return (
+    <input
+      type="button"
+      value="Theme"
+      className={`button ${theme}`}
+      onClick={onUpdateTheme}
+    />
+  );
 }
 
 root.render(<Button />);
